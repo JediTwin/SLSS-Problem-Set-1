573d22b728521b2994d03620294ab69808d9c926
Noel Rebiffe <jeditwin@icloud.com>
2021-09-27 13:30:42 -0700

    Added question 4

--- rebiffe_noel_p1q1.py
+++ rebiffe_noel_p1q1.py
@@ -18,7 +18,7 @@ if q1_answer > 11:
     print("A little too high there.")
 elif q1_answer < 11:
     print("Just a bit higher.")
-else:
+elif q1_answer == 11:
     print("Correct!")
     score += 1
 
@@ -30,7 +30,7 @@ print("A: Worcestershire")
 print("B: Edinburgh")
 print("or C: London")
 
-q2_answer = input().upper()
+q2_answer = input().upper().strip(".")
 
 if q2_answer == "A":
     print("Really?")
@@ -45,12 +45,32 @@ time.sleep(2)
 
 # Question 3
 print("Question 3: What was Game of the Year in 2020?")
-q3_answer = input().lower()
+q3_answer = input().lower().strip(".")
 
 if q3_answer == "the last of us part 2" or "the last of us part II" or "the last of us 2":
     print("Correct!")
     score += 1
 elif q3_answer == "hades":
     print("I wish...")
+else:
+    print("That's not it...")
 
 time.sleep(2)
+
+# Question 4
+print("Question 4!")
+print("What is the chemical name of H2O2?")
+q4_answer = input().lower().strip(".")
+
+if q4_answer == "Hydrogen Peroxide":
+    print("Correct!")
+    score += 1
+elif q4_answer == "Water":
+    print("That's H2O...")
+else:
+    print("What?")
+
+
+# Question 5
+print("Final Question!")
+print("")
